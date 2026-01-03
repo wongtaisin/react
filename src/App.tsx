4768d43728001c1026b2bda03327fef97dac7701
wingddd <wongtaisin1024@gmail.com>
2026-01-03 15:16:43 +0800

    设置全局中文

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { ConfigProvider } from 'antd'
+import zhCN from 'antd/locale/zh_CN'
 import { RouterProvider } from 'react-router-dom'
 import './App.css'
 import router from './router'
 
 const App = () => {
-  return <RouterProvider router={router} />
+  return (
+    <ConfigProvider locale={zhCN}>
+      <RouterProvider router={router} />
+    </ConfigProvider>
+  )
 }
 
 export default App
